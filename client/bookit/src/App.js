b96f3296d99b93af4653a08d657732b3f5344bf8
Madison Henry <packback2000@users.noreply.github.com>
2022-03-11 20:42:41 -0500

    commit

--- client/bookit/src/App.js
+++ client/bookit/src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
     <div>
       <BrowserRouter>
-      
+      <DarkMode></DarkMode>
       <Routes>
         
         <Route path="/" element={ <HomePage />}></Route>
