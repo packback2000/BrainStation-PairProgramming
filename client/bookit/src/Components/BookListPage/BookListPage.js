2d6ca4383b0c12a825a77645875ea909634efed4
Madison Henry <packback2000@users.noreply.github.com>
2022-03-12 15:23:27 -0500

    commit

--- client/bookit/src/Components/BookListPage/BookListPage.js
+++ client/bookit/src/Components/BookListPage/BookListPage.js
@@ -11,7 +11,7 @@ export default function BookListPage(props) {
          
             <p className="book-rating">Rating: {(props.rating !== undefined) ? props.rating : "Not Available"}</p>
             <p className="rating-count">Number of Ratings: {(props.ratingCount !== undefined) ? props.ratingCount : "Not Available"}</p>
-            <p className="book-price">Price: {(props.price !== undefined) ? props.price : "Not Available"}</p>
+            <p className="book-price">Price: {(props.price !== -1) ? props.price : "Not Available"}</p>
         </section>
     )
 
