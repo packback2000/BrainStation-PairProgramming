2a56a507c6c0fceb5cca5bde9484f3374f204cc8
Madison Henry <packback2000@users.noreply.github.com>
2022-03-11 20:12:13 -0500

    commit

--- client/bookit/src/App.js
+++ client/bookit/src/App.js
@@ -2,15 +2,25 @@
 import HomePage from "./Components/HomePage/HomePage";
 import React from 'react';
 import DarkMode from "./DarkMode";
-
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
+import { Switch } from 'react-router-dom'
+import BookListPage from "./Components/BookListPage/BookListPage";
 
 function App() {
 
   
   return (
     <div>
-      <DarkMode />
-      <HomePage />
+      <BrowserRouter>
+      
+      <Routes>
+        
+        <Route path="/" element={ <HomePage />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
     
   );
