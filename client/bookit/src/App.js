0f3da672e73749776766cc7d36e4833c8ea29aea
Madison Henry <packback2000@users.noreply.github.com>
2022-03-11 23:23:42 -0500

    commit

--- client/bookit/src/App.js
+++ client/bookit/src/App.js
@@ -2,12 +2,10 @@
 import HomePage from "./Components/HomePage/HomePage";
 import React from 'react';
 import DarkMode from "./DarkMode";
-import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import { Routes } from 'react-router-dom';
-import { Switch } from 'react-router-dom'
-import BookListPage from "./Components/BookListPage/BookListPage";
+
 
 function App() {
 
--- client/bookit/src/Components/BookListPage/BookListPage.js
+++ client/bookit/src/Components/BookListPage/BookListPage.js
@@ -1,15 +1,15 @@
 
 
 export default function BookListPage(props) {
+    let url = 'http://lgimages.s3.amazonaws.com/nc-md.gif';
     return(
         <section className="booklist">
-           
-                <p className="book-title">{props.title}</p>
-                <p className="book-title">{props.author}</p>
-                <a href='{props.url}'> </a>
-                <img src={props.image} alt="book cover" />
-                <p className="book-title">{props.description}</p>
-                <p className="book-title">{props.rank}</p>
+            <p className="book-title">{(props.title !== undefined) ? props.title : "Title Not Available"}</p>
+            <p className="book-author">{(props.author !== undefined) ? props.author : "Author Not available"}</p>
+            <img src={(props.image !== undefined) ? props.image : url} alt="book cover" />
+            <p className="book-description"> { (props.description !== undefined) ?  props.description : "Description Not Available" } </p>
+            <p className="book-rating">Rating: {(props.rating !== undefined) ? props.rating : "Not Available"}</p>
+            <p className="rating-count">Number of Ratings: {(props.ratingCount !== undefined) ? props.ratingCount : "Not Available"}</p>
         </section>
     )
 }
