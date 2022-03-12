2e9cfa122c5b303441a16c2a38cc6c8c2afedf7d
Madison Henry <packback2000@users.noreply.github.com>
2022-03-12 13:32:34 -0500

    commit

--- client/bookit/src/App.js
+++ client/bookit/src/App.js
@@ -7,19 +7,24 @@ import { Route } from 'react-router-dom';
 import { Routes } from 'react-router-dom';
 import Header from "./Components/Header/Header";
 import BookListPage from "./Components/BookListPage/BookListPage";
+import { useParams } from "react-router-dom";
 
 
 function App() {
 
-  
+  let params = useParams();
+  console.log(params)
+
   return (
     <div>
       <BrowserRouter>
       <DarkMode></DarkMode>
       <Header />
       <Routes>
+      
         <Route path="/" element={ <HomePage />}></Route>
-        <Route path="/search/:bookID" element = {<BookListPage /> } />
+        <Route path="/search" element={<HomePage />}></Route>
+        <Route path="/search/:bookID" element={<BookListPage />} />
       </Routes>
       </BrowserRouter>
     </div>
--- client/bookit/src/Components/BookListPage/BookListPage.js
+++ client/bookit/src/Components/BookListPage/BookListPage.js
@@ -12,6 +12,7 @@ export default function BookListPage(props) {
             <p className="book-description"> { (props.description !== undefined) ?  props.description : "Description Not Available" } </p>
             <p className="book-rating">Rating: {(props.rating !== undefined) ? props.rating : "Not Available"}</p>
             <p className="rating-count">Number of Ratings: {(props.ratingCount !== undefined) ? props.ratingCount : "Not Available"}</p>
+            <p className="book-price">Price: {(props.price !== undefined) ? props.price : "Not Available"}</p>
             </Link>
         </section>
     )
